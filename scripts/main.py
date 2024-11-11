13ce87c20d7813b5b26816b7c02e6429957cded0
Liu Zhongwei <liuzhongwei@espressif.com>
2024-11-11 09:54:49 +0800

    feat(scripts): update

--- scripts/main.py
+++ scripts/main.py
@@ -3,8 +3,9 @@ import os
 import gc
 import _thread
 import lvgl as lv
-import SDL
+import lv_utils
 import esp_brookesia as brookesia
+from display_driver_utils import driver
 
 def run_python_file(file_path):
     with open(file_path) as f:
@@ -16,26 +17,7 @@ def run_python_file(file_path):
 # Initialize LVGL and SDL
 print("Initializing LVGL and SDL...")
 lv.init()
-SDL.init()
-
-print("Registering display and mouse drivers...")
-# Register SDL display driver.
-draw_buf = lv.disp_draw_buf_t()
-buf1_1 = bytearray(480*10)
-draw_buf.init(buf1_1, None, len(buf1_1)//4)
-disp_drv = lv.disp_drv_t()
-disp_drv.init()
-disp_drv.draw_buf = draw_buf
-disp_drv.flush_cb = SDL.monitor_flush
-disp_drv.hor_res = 800
-disp_drv.ver_res = 480
-disp_drv.register()
-# Regsiter SDL mouse driver
-indev_drv = lv.indev_drv_t()
-indev_drv.init()
-indev_drv.type = lv.INDEV_TYPE.POINTER
-indev_drv.read_cb = SDL.mouse_read
-indev_drv.register()
+drv = driver(800, 480)
 
 print( "Free memory: " + str(gc.mem_free()) )
 
